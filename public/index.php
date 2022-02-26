265d66e55f52a1f7c5e956bfc933d82ec8fb5723
Raul Garcia <raulgarciacanet@gmail.com>
2022-02-26 20:10:59 +0100

    step3-manual-loader
    - Add default controller and load it manually
    - Fix docker-compose

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "HELLO FROM PROJECT";
+use App\Controllers\HomeController;
+
+require_once "../src/app/Controllers/HomeController.php";
+
+$greeting = new HomeController();
+$greeting->sayHello('Raul');
---
+++ src/app/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Controllers;
+
+class HomeController {
+
+  public function __construct() {
+  }
+
+  public function sayHello(string $name): void {
+    echo "Hello $name";
+  }
+}
