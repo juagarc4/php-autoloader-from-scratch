05c7a7e0fb8e951218f5bb3ed7657a1a1dec0d9a
Raul Garcia <raulgarciacanet@gmail.com>
2022-03-06 13:52:11 +0100

    step9-get-post-routes
    - Add management of post and get routes

--- public/index.php
+++ public/index.php
@@ -8,7 +8,15 @@ $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $router = new App\Router();
-$router->register('/', [App\Controllers\HomeController::class, 'index']);
-$router->register('/users', [App\Controllers\UsersController::class, 'index']);
+$router->get('/', [App\Controllers\HomeController::class, 'index']);
+$router->get('/users', [App\Controllers\UsersController::class, 'index']);
+$router->get(
+  '/users/create',
+  [App\Controllers\UsersController::class, 'create']
+);
+$router->post(
+  '/users/create',
+  [App\Controllers\UsersController::class, 'save']
+);
 
-echo $router->resolve($_SERVER['REQUEST_URI']);
+echo $router->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
--- src/app/Controllers/UsersController.php
+++ src/app/Controllers/UsersController.php
@@ -5,9 +5,26 @@ namespace App\Controllers;
 class UsersController
 {
 
-    public static function index()
+    public function index()
     {
         return "USERS";
+    }
+
+    public function create(): string
+    {
+        return '<form action="/users/create" method="post" name="form_user_create">
+        <label>Username</label><input type="text" name="user_name"/>
+        <label>Password</label><input type="password" name="user_password"/>
+        <button>Save</button>
+        </form>';
+    }
+
+    public function save(): void
+    {
+        $userName = $_POST['user_name'];
+        $userPassword = $_POST['user_password'];
+
+        var_dump($userName, $userPassword);
     }
 
 }
