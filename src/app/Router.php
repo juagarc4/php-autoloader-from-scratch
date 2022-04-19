db8f2ce6d36acb1b9d5b3b393ce2d4d97b66c896
Raul Garcia <raulgarciacanet@gmail.com>
2022-04-19 09:46:52 +0200

    step-13-create-first-unit-test

--- src/app/Router.php
+++ src/app/Router.php
@@ -6,63 +6,58 @@ namespace App;
 
 use App\Exceptions\RouteNotFoundException;
 
-class Router
-{
+class Router {
 
-    private array $routes;
+  private array $routes = [];
 
-    public function get(string $route, callable|array $action): self
-    {
-        $this->register('GET', $route, $action);
-        return $this;
+  public function get(string $route, callable|array $action): self {
+    $this->register('GET', $route, $action);
+    return $this;
+  }
+
+  public function register(
+    string $requestMethod,
+    string $route,
+    callable|array $action
+  ): self {
+    $this->routes[$requestMethod][$route] = $action;
+    return $this;
+  }
+
+  public function post(string $route, callable|array $action): self {
+    $this->register('POST', $route, $action);
+    return $this;
+  }
+
+  public function routes(): array {
+    return $this->routes;
+  }
+
+  /**
+   * @throws \App\Exceptions\RouteNotFoundException
+   */
+  public function resolve(string $requestUri, string $requestMetod) {
+    $route = explode('?', $requestUri)[0];
+    $action = $this->routes[$requestMetod][$route] ?? NULL;
+
+    if (!$action) {
+      throw new RouteNotFoundException();
     }
 
-    public function register(
-      string $requestMethod,
-      string $route,
-      callable|array $action
-    ): self {
-        $this->routes[$requestMethod][$route] = $action;
-        return $this;
+    if (is_callable($action)) {
+      return $action();
     }
 
-    public function post(string $route, callable|array $action): self
-    {
-        $this->register('POST', $route, $action);
-        return $this;
-    }
-
-    public function routes(): array
-    {
-        return $this->routes;
-    }
-
-    /**
-     * @throws \App\Exceptions\RouteNotFoundException
-     */
-    public function resolve(string $requestUri, string $requestMetod)
-    {
-        $route = explode('?', $requestUri)[0];
-        $action = $this->routes[$requestMetod][$route] ?? null;
-
-        if (!$action) {
-            throw new RouteNotFoundException();
+    if (is_array($action)) {
+      [$class, $method] = $action;
+      if (class_exists($class)) {
+        $class = new $class;
+        if (method_exists($class, $method)) {
+          return $class->$method([]);
         }
-
-        if (is_callable($action)) {
-            return $action();
-        }
-
-        if (is_array($action)) {
-            [$class, $method] = $action;
-            if (class_exists($class)) {
-                $class = new $class;
-                if (method_exists($class, $method)) {
-                    return $class->$method([]);
-                }
-            }
-        }
-        throw new RouteNotFoundException();
+      }
     }
+    throw new RouteNotFoundException();
+  }
 
 }
--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -1,5 +1,125 @@
 <?php
 
-class RouterTest {
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Exceptions\RouteNotFoundException;
+use App\Router;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase {
+
+  private Router $router;
+
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->router = new Router();
+  }
+
+  /** @test */
+  public function it_registers_a_route(): void {
+    $this->router->register('GET', '/users', ['Users', 'index']);
+
+    $expected = [
+      'GET' => [
+        '/users' => ['Users', 'index'],
+      ],
+    ];
+
+    $this->assertSame($expected, $this->router->routes());
+  }
+
+  /** @test */
+  public function it_registers_a_get_route(): void {
+    $this->router->get('/users', ['Users', 'index']);
+
+    $expected = [
+      'GET' => [
+        '/users' => ['Users', 'index'],
+      ],
+    ];
+
+    $this->assertSame($expected, $this->router->routes());
+  }
+
+  /** @test */
+  public function it_registers_a_post_route(): void {
+    $this->router->post('/users', ['Users', 'store']);
+
+    $expected = [
+      'POST' => [
+        '/users' => ['Users', 'store'],
+      ],
+    ];
+
+    $this->assertSame($expected, $this->router->routes());
+  }
+
+  /** @test */
+  public function there_are_no_routes_when_router_is_created(): void {
+    $this->assertEmpty((new Router())->routes());
+  }
+
+  /**
+   * @test
+   * @dataProvider routeNotFoundCases
+   */
+  public function it_throws_route_not_found_exception(
+    string $requestUri,
+    string $requestMethod
+  ): void {
+    $users = new class() {
+
+      public function delete(): bool {
+        return TRUE;
+      }
+
+    };
+
+    $this->router->post('/users', [$users::class, 'store']);
+    $this->router->get('/users', ['Users', 'index']);
+
+    $this->expectException(RouteNotFoundException::class);
+    $this->router->resolve($requestUri, $requestMethod);
+  }
+
+  public function routeNotFoundCases(): array {
+    return [
+      ['/users', 'PUT'],
+      ['/invoices', 'OST'],
+      ['/users', 'GET'],
+      ['/users', 'POST'],
+    ];
+  }
+
+  /** @test */
+  public function it_resolves_route_from_a_closure(): void {
+    $this->router->get('/users', fn() => [1, 2, 3]);
+
+    $this->assertSame(
+      [1, 2, 3],
+      $this->router->resolve('/users', 'GET')
+    );
+  }
+
+  /** @test */
+  public function it_resolves_route(): void {
+    $users = new class() {
+
+      public function index(): array {
+        return [1, 2, 3];
+      }
+
+    };
+
+    $this->router->get('/users', [$users::class, 'index']);
+
+    $this->assertSame(
+      [1, 2, 3],
+      $this->router->resolve('/users', 'GET')
+    );
+  }
 
 }
